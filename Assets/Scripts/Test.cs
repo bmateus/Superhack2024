9f7d99ced7e87e23adb2a8add942142dd335b0e2
bmateus <bmateus@gmail.com>
2024-08-04 13:11:18 -0400

    Set up Unity Project

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Thirdweb;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello!");
+
+    }
+
+}
